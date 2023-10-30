69178453a93cdf2d67f78417caecd38b6eb96c23
LeeGwangPyo <148610877+LeeGwangPyo@users.noreply.github.com>
2023-10-30 16:41:55 +0900

    2023.10.30 2nd

---
+++ PointConsoleOutput.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+
+class Point
+{
+private:
+	int xpos, ypos;
+public:
+	Point(int x = 0, int y = 0) :xpos(x), ypos(y)
+	{ }
+	void ShowPosition()const
+	{
+		std::cout << '[' << xpos << ', ' << ypos << ']' << std::endl;
+	}
+	friend ostream& operator<<(ostream& os, const Point& pos);
+};
+
+ostream& operator<<(ostream& os, const Point pos)
+{
+	os << '[' << pos.xpos << ", " << pos.ypos << ']' << std::endl;
+	return os;
+}
+
+int main(void)
+{
+	Point pos1(1, 3);
+	cout << pos1;
+	Point pos2(101, 303);
+	cout << pos2;
+	return 0;
+}
