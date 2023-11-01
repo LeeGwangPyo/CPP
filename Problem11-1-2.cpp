0508e2b75a742997786fd076afdbed97d893b01e
LeeGwangPyo <148610877+LeeGwangPyo@users.noreply.github.com>
2023-11-01 15:41:57 +0900

    2023.11.01 1st

--- Problem11-1-2.cpp
+++ Problem11-1-2.cpp
@@ -15,6 +15,13 @@ public:
 		strcpy(title, Ttitle);
 		strcpy(isbn, Tisbn);
 	}
+	Book(const Book& ref) :price(ref.price)
+	{
+		title = new char[strlen(ref.title) + 1];
+		isbn = new char[strlen(ref.isbn) + 1];
+		strcpy(title, ref.title);
+		strcpy(isbn, ref.isbn);
+	}
 	void ShowBookInfo()
 	{
 		std::cout << "제목: " << title << std::endl;
@@ -24,10 +31,15 @@ public:
 
 	Book& operator=(const Book& ref)
 	{
-		title = new char[strlen(ref.Ttitle) + 1];
-		isbn = new char[strlen(ref.Tisbn) + 1];
+		delete[]title;
+		delete[]isbn;
+
+		title = new char[strlen(ref.title) + 1];
+		isbn = new char[strlen(ref.isbn) + 1];
 		strcpy(title, ref.title);
 		strcpy(isbn, ref.isbn);
+		price = ref.price;
+		return *this;
 	}
 };
 
@@ -41,18 +53,34 @@ public:
 		DRMKey = new char[strlen(Drm) + 1];
 		strcpy(DRMKey, Drm);
 	}
+	EBook(const EBook& ref) :Book(ref)
+	{
+		DRMKey = new char[strlen(ref.DRMKey) + 1];
+		strcpy(DRMKey, ref.DRMKey);
+	}
 	void ShowEBookInfo()
 	{
 		ShowBookInfo();
 		std::cout << "인증키: " << DRMKey << std::endl;
 	}
+	EBook& operator=(const EBook& ref)
+	{
+		delete[]DRMKey;
+		Book::operator=(ref);
+		DRMKey = new char[strlen(ref.DRMKey) + 1];
+		strcpy(DRMKey, ref.DRMKey);
+		return *this;
+	}
 };
 int main(void)
 {
-	Book book("좋은 C++", "555-12345-890-0", 20000);
-	book.ShowBookInfo();
+	Book book1("좋은 C++", "555-12345-890-0", 20000);
+	Book book2 = book1;
+	book2.ShowBookInfo();
 	std::cout << std::endl;
-	EBook ebook("좋은 C++ Ebook", "555-12345-890-1", 10000, "fdx9w0i8kiw");
-	ebook.ShowEBookInfo();
+	EBook ebook1("좋은 C++ Ebook", "555-12345-890-1", 10000, "fdx9w0i8kiw");
+	EBook ebook2("좋은 C++", "555-12345-890-0", 20000, "dfex2w7iw1u");
+	ebook2 = ebook1;
+	ebook2.ShowEBookInfo();
 	return 0;
 }
