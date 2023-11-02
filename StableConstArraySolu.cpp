9446ea7ee1fa30481113df134299c41e2afaab1f
LeeGwangPyo <148610877+LeeGwangPyo@users.noreply.github.com>
2023-11-02 15:20:02 +0900

    2023.11.02 1st

--- StableConstArraySolu.cpp
+++ StableConstArraySolu.cpp
@@ -6,15 +6,15 @@ class BoundCheckIntArray
 private:
 	int* arr;
 	int arrlen;
-	BoundCheckIntArray(const BoundCheckIntArray& arr) {};
-	BoundCheckIntArray& operator=(const BoundCheckIntArray& arr){}
+	BoundCheckIntArray(const BoundCheckIntArray& arr) {}
+	BoundCheckIntArray& operator=(const BoundCheckIntArray& arr) {}
 
 public:
 	BoundCheckIntArray(int len) :arrlen(len)
 	{
 		arr = new int[len];
 	}
-	int& operator[](int idx) const
+	int& operator[](int idx)
 	{
 		if (idx < 0 || idx >= arrlen)
 		{
@@ -23,10 +23,19 @@ public:
 		}
 		return arr[idx];
 	}
-
+	int operator[](int idx)const
+	{
+		if (idx < 0 || idx >= arrlen)
+		{
+			std::cout << "Array index out of bound exception" << std::endl;
+			exit(1);
+		}
+		return arr[idx];
+	}
 	int GetArrLen()const { return arrlen; }
 	~BoundCheckIntArray() { delete[]arr; }
 };
+
 void ShowAllData(const BoundCheckIntArray& ref)
 {
 	int len = ref.GetArrLen();
