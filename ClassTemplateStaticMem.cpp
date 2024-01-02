90e568e1074c79a1275800cf65946608922f09da
LeeGwangPyo <148610877+LeeGwangPyo@users.noreply.github.com>
2024-01-02 15:52:36 +0900

    24.01.02 1st

---
+++ ClassTemplateStaticMem.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+
+template<typename T>
+class SimpleStaticMem
+{
+private:
+	static T mem;
+public:
+	void AddMem(T num) { mem += num; }
+	void ShowMem() { std::cout << mem << std::endl; }
+};
+
+template <typename T>
+T SimpleStaticMem<T>::mem = 0;
+
+int main(void)
+{
+	SimpleStaticMem<int> obj1;
+	SimpleStaticMem<int> obj2;
+	obj1.AddMem(2);
+	obj2.AddMem(3);
+	obj1.ShowMem();
+	SimpleStaticMem<long> obj3;
+	SimpleStaticMem<long> obj4;
+	obj3.AddMem(100);
+	obj4.ShowMem();
+	return 0;
+}
